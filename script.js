d39f3b4489b0b326cd8be66f571ea57cd3a261d3
Srivatsan Talkad <59266307+srivatsantalkad@users.noreply.github.com>
2021-11-03 05:31:45 +0000

    Edit playRound function in script.js

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 function computerPlay() {
+	// These are the choices that the computer can choose from.
 	let choices = ["Rock", "Paper", "Scissors"];
-	let choice = choices[Math.floor((Math.random() * 3) + 1) - 1];
+
+	// Randomly pick Rock, Paper, or Scissors, that will be the computer's choice.
+    let choice = choices[Math.floor((Math.random() * 3) + 1) - 1];
+	
 	return choice;
 }
 
-function playRound() {
+function playRound(playerSelection, computerSelection) {
+
+	// Convert the player's and computer's selection to lower case for simplicity in checking their choices.
+	playerSelection = playerSelection.toUpperCase();
+	computerSelection = computerSelection.toUpperCase();
+
+	// Check whether player won, lost, or drew to computer.
 	
+	// Check whether player won.
+	if ((playerSelection == "ROCK" && computerSelection == "SCISSORS")
+	|| (playerSelection == "SCISSORS" && computerSelection == "PAPER")
+	|| (playerSelection == "PAPER" && computerSelection == "ROCK")) {
+		// The victory message indicating that the user won.
+		let victoryMessage = "You won! " + playerSelection + " beats " + computerSelection;
+		return victoryMessage;
+	} 
+	// Check whether player drew.
+	else if (playerSelection == computerSelection) {
+		// The draw message indicating that the user drew.
+		let drawMessage = "You drew! " + playerSelection + " ties with " + computerSelection;
+		return drawMessage;
+	} 
+	// If neither of the conditionals are true, then the user lost.
+
+	// The lose message indicating that the user lost.
+	let loseMessage = "You lose! " + computerSelection + " beats " + playerSelection;
+	
+	return loseMessage;
 }
