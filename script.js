3f1b57a2bd8829e2c49c485aaa154ee68b11d0eb
Srivatsan Talkad <59266307+srivatsantalkad@users.noreply.github.com>
2021-11-03 06:35:28 +0000

    Edit function game() in script.js, create function getGameResult() in script.js, create function updateScore in script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function computerPlay() {
 	let choices = ["Rock", "Paper", "Scissors"];
 
 	// Randomly pick Rock, Paper, or Scissors, that will be the computer's choice.
-    let choice = choices[Math.floor((Math.random() * 3) + 1) - 1];
-	
+	let choice = choices[Math.floor((Math.random() * 3) + 1) - 1];
+
 	return choice;
 }
 
@@ -14,17 +14,17 @@ function playRound(playerSelection, computerSelection) {
 	// Convert the player's and computer's selection to uppercase, for simplicity in determining winner and loser.
 	playerSelection = playerSelection.toUpperCase();
 	computerSelection = computerSelection.toUpperCase();
-	
+
 	// Check whether player won.
 	if ((playerSelection == "ROCK" && computerSelection == "SCISSORS")
-	|| (playerSelection == "SCISSORS" && computerSelection == "PAPER")
-	|| (playerSelection == "PAPER" && computerSelection == "ROCK")) {
+		|| (playerSelection == "SCISSORS" && computerSelection == "PAPER")
+		|| (playerSelection == "PAPER" && computerSelection == "ROCK")) {
 
 		// The victory message indicating that the user won.
 		let victoryMessage = "You won! " + playerSelection + " beats " + computerSelection;
 
 		return victoryMessage;
-	} 
+	}
 
 	// Check whether player drew.
 	else if (playerSelection == computerSelection) {
@@ -33,14 +33,60 @@ function playRound(playerSelection, computerSelection) {
 		let drawMessage = "You drew! " + playerSelection + " ties with " + computerSelection;
 
 		return drawMessage;
-	} 
+	}
 
 	// If neither of the conditionals are true, then the user lost.
 
 	// The lose message indicating that the user lost.
 	let loseMessage = "You lose! " + computerSelection + " beats " + playerSelection;
-	
+
 	return loseMessage;
+}
+
+function updateScore(score, roundResultMessage, isUser) {
+
+	// Get 4th character of the round result message, this will be either "l" or "w": l = user lost, w = user won.
+	const STARTING_INDEX_FOR_RESULT = 4;
+	const ENDING_INDEX_FOR_RESULT = 5;
+	let roundResultCharacter = roundResultMessage.substring(STARTING_INDEX_FOR_RESULT, ENDING_INDEX_FOR_RESULT);
+
+	// Determine whether to increment score by 1.
+	// If user won.
+	if (roundResultCharacter == "w" && isUser) {
+
+		// Add 1 to user's total score.
+		score++;
+	}
+	// If user lost.
+	else if (roundResultCharacter == "l" && !isUser) {
+
+		// Add 1 to computer's total score.
+		score++;
+	}
+	// If the two above conditions are not true, then user and computer drew, no need to do anything.
+	return score;
+}
+
+function getGameResult(userTotalScore, computerTotalScore) {
+	// If user won best of 5.
+	if (userTotalScore > computerTotalScore) {
+
+		// Indicate in game result message that user won.
+		gameResultMessage = "You won the game!";
+	}
+	// If game was a draw.
+	else if (userTotalScore == computerTotalScore) {
+
+		// Indicate in game result message that user and computer drew.
+		gameResultMessage = "The game is a draw!";
+	}
+	// If the above two conditions are not true, then the user lost the game.
+	else {
+
+		// Indicate in game result message that user lost.
+		gameResultMessage = "You lost the game!";
+	}
+	return gameResultMessage
 }
 
 function game() {
@@ -59,58 +105,28 @@ function game() {
 
 	// Play for 5 rounds
 	for (let roundNumber = 1; roundNumber <= NUMBER_OF_ROUNDS; ++roundNumber) {
-		
+
 		// Before playing each round, get choice from user.
 		let userChoice = prompt("Enter your choice (rock, paper, scissors): ");
-		
+
 		// Get computer's choice.
 		let computerChoice = computerPlay();
-		
-		// Play the round, function returns result message.
+
+		// Play the round, function returns round result message.
 		let roundResultMessage = playRound(userChoice, computerChoice);
-		
+
 		// Display the results of the round.
 		console.log(roundResultMessage);
 
-		// Get 4th character of the round result message, this will be either "l" or "w": l = user lost, w = user won.
-		const STARTING_INDEX_FOR_RESULT = 4;
-		const ENDING_INDEX_FOR_RESULT = 5;
-		let roundResultCharacter = roundResultMessage.substring(STARTING_INDEX_FOR_RESULT, ENDING_INDEX_FOR_RESULT);
-
-		// Determine whether to increment userScore or computerScore, by 1.
-		// If user won.
-		if (roundResultCharacter == "w") {
-			
-			// Add 1 to user's total score.
-			userTotalScore++;
-		}
-		// If user lost.
-		else if (roundResultCharacter == "l") {
-
-			// Add 1 to computer's total score.
-			computerTotalScore++;
-		}
-		// If the two above conditions are not true, then user and computer drew, no need to do anything.
+		// Update scores of user and computer.
+		const IS_USER = true;
+		const IS_NOT_USER = false;
+		userTotalScore = updateScore(userTotalScore, roundResultMessage, IS_USER);
+		computerTotalScore = updateScore(computerTotalScore, roundResultMessage, IS_NOT_USER);
 	}
 	// Calculate who won the best of 5.
-	// If user won best of 5.
-	if (userTotalScore > computerTotalScore) {
+	gameResultMessage = getGameResult(userTotalScore, computerTotalScore);
 
-		// Indicate in game result message that user won.
-		gameResultMessage = "You won the game!";
-	} 
-	// If game was a draw.
-	else if (userTotalScore == computerTotalScore) {
-
-		// Indicate in game result message that user and computer drew.
-		gameResultMessage = "The game is a draw!";
-	}
-	// If the above two conditions are not true, then the user lost the game.
-	else {
-
-		// Indicate in game result message that user lost.
-		gameResultMessage = "You lost the game!";
-	}
 	// Display to the user the result.
 	console.log(gameResultMessage);
 }
